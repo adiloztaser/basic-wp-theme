2a158ee60a594bb4ca5ebdf1f0abfb7f97f0eaae
adil öztaşer <adiloztaser@gmail.com>
2015-09-15 15:37:15 +0300

    add 404 page

---
+++ 404.php
@@ -0,0 +1,12 @@
+<?php get_header(); ?>
+
+
+	<ul class="post-list">
+		<h4 class="not-found">404</h4>
+		<h2 class="text-center"><a href="<?php bloginfo('url'); ?>">Home</a></h2>
+	</ul>
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
