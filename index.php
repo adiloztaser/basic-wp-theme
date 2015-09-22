4c24c9eed7cc1decce2c7e885c093f209a257280
adil öztaşer <adiloztaser@gmail.com>
2015-09-22 12:58:00 +0300

    exclude category from homepage

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 
 
 	<ul class="post-list">
+		<?php query_posts('cat=-15'); ?>
     <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 		<li>
 			<div class="post-date"><?php the_time('d/m/Y'); ?></div><div class="post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></div>
