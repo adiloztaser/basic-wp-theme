94ff75a13cff33d6124ac3d5a6daaffcd80dd3b9
adil öztaşer <adiloztaser@gmail.com>
2015-10-12 18:48:35 +0300

    query removed

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php get_header(); ?>
 	<ul class="post-list">
-		<?php query_posts('cat=-15'); ?>
     <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
 		<li>
@@ -9,7 +8,7 @@
 				<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
 			</div>
 		</li>
-		
+
   <?php endwhile; else: ?>
   <?php _e('Clear'); ?>
   <?php endif; ?>
