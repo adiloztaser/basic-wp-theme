9934e8388a97384eca562bad9f0e698f6632b82d
Adil ÖZTAŞER <a@oztaser.com>
2015-12-19 14:29:57 +0200

    added js file link

--- header.php
+++ header.php
@@ -11,6 +11,8 @@
   <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,600,700,400italic' rel='stylesheet' type='text/css'>
   <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" />
   <?php wp_head(); ?>
+  <script type='text/javascript' src='<?php bloginfo('template_url'); ?>/js/jquery.js'></script>
+  <script type='text/javascript' src='<?php bloginfo('template_url'); ?>/js/pagination.js'></script>
 </head>
 <body>
 <div class="grid grid-pad">
